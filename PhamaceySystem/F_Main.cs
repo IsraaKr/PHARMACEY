5374bce2ade985278f6c88b50000177effc1a002
Israa <Israa@ISRAA-PC>
2023-12-13 04:36:58 +0200

    Add project files.

---
+++ PhamaceySystem/F_Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+
+namespace PhamaceySystem
+{
+    public partial class F_Main : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public F_Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
